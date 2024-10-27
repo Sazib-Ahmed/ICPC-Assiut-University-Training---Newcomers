f266c0b857ce8fd1b42ed90f485a57b439d9765f
Sazib-Ahmed <mail.sazibahmed@gmail.com>
2024-10-27 19:44:29 +0600

    Refactor code for sorting an array in C

---
+++ sheet_#3_(arrays)/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+    // for (int i = 0; i < n - 1; i++) {
+    //     for (int j = i + 1; i < n; j++) {
+    //         if (a[i] > a[j]) {
+    //             int temp = a[i];
+    //             a[i] = a[j];
+    //             a[j] = temp;
+    //         }
+    //     }
+    // }
